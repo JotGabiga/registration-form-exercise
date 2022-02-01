530361e9c7a6c3d25de54e81b219d3abd9831fb0
Justyna Gabiga <jot.gabiga@gmail.com>
2022-02-01 17:53:18 +0100

    Unit test written for component rendering

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -2,34 +2,32 @@ import React from "react";
 
 const RegistrationForm = () => {
   return (
-    <section className="mainContainer">
       <form>
         <h1>Rejestracja</h1>
 
-        <div className="filed">
-          <label>Imię</label><br></br>
-          <input type="text" name="username" placeholder="Username" autocomplete="off"/>
+        <div className="field">
+          <label htmlFor="username">Imię</label><br></br>
+          <input type="text" name="username" placeholder="Username"/>
         </div>
 
-        <div className="filed">
-          <label>Hasło</label><br></br>
-          <input type="password" name="password" placeholder="Password" autocomplete="new-password" />
+        <div className="field">
+          <label htmlFor="password">Hasło</label><br></br>
+          <input type="password" name="password" placeholder="Password" autoComplete="off" />
         </div>
 
-        <div className="filed">
-          <input type="checkbox" id=" newsletter" name=" newsletter" value=" newsletter" />
-          <label for="newsletter"> Zgoda na newsletter</label>
+        <div className="field">
+          <input type="checkbox" id="newsletter" name="newsletter"/>
+          <label htmlFor="newsletter">Zgoda na newsletter</label>
         </div>
 
-        <div className="filed">
-          <label>Email</label><br></br>
+        <div className="field">
+          <label htmlFor="email">Email</label><br></br>
           <input type="email" name="email" placeholder="Email" />
         </div>
 
         <button>Wyślij</button>
 
       </form>
-    </section>
   );
 };
 export default RegistrationForm;
---
+++ src/components/RegistrationForm.test.js
@@ -0,0 +1,35 @@
+import RegistrationForm from "./RegistrationForm";
+import ReactDOM from "react-dom";
+import React from "react";
+
+describe("<RegistrationForm />", () => {
+  const root = document.createElement("div");
+  ReactDOM.render(<RegistrationForm />, root);
+
+  test("renders the correct content", () => {
+    expect(root.querySelector('form')).toBeTruthy()
+    expect(root.querySelector("h1").textContent).toBe("Rejestracja");
+
+    let fields = root.getElementsByClassName ("field");
+    expect(fields.length).toBe(4);
+
+    expect(fields[0].querySelector("label").textContent).toBe("Imię");
+    expect(fields[0].querySelector("input").value).toBe("");
+
+    expect(fields[1].querySelector("label").textContent).toBe("Hasło");
+    expect(fields[1].querySelector("input").value).toBe("");
+
+    expect(fields[2].querySelector("label").textContent).toBe(
+      "Zgoda na newsletter"
+    );
+    expect(fields[2].querySelector("input").checked).toEqual(false);
+
+    expect(fields[3].querySelector("label").textContent).toBe("Email");
+    expect(fields[3].querySelector("input").value).toBe("");
+
+    expect(root.querySelector('button').textContent).toBe("Wyślij")
+  });
+
+  // Test 2
+  test("", () => {});
+});
